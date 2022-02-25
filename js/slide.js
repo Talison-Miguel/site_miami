f1d6e4b8c13132473f1dde18af8739ffc900c87e
Tálison Miguel <talisonmiguel84@gmail.com>
2022-02-25 09:03:52 -0300

    Melhorias no site

---
+++ js/slide.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.carrosel').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
